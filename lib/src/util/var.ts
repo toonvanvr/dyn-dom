567b445cf8705ab154e96b02781c5d103338e550
Toon Vanvreckem <toon.vanvreckem@gmail.com>
2022-10-01 15:20:47 +0200

    feat: pass hook management interface to action
    - allows hook lifecycle management during execution

--- lib/src/util/var.ts
+++ lib/src/util/var.ts
@@ -1,21 +1,66 @@
 import { nil, Nil } from "./symbols.js";
 
-export type Hook<T> = (value: T) => void;
+type UnhookFn = () => void;
+type ActionFn<T> = (value: T, hook: Hook<T>) => void;
+export interface Hook<T> {
+  action: ActionFn<T>;
+  unhook: UnhookFn;
+}
 
 export class Var<T> {
-  private hooks = new Set<Hook<T>>();
+  #value: T | Nil;
+  #hooks = new Set<Hook<T>>();
+  #next: Promise<T> | null = null;
 
-  constructor(private value: Nil | T = nil) {}
+  constructor(value: T | Nil = nil) {
+    this.#value = value;
+  }
 
-  hook(hook: Hook<T>): () => void {
-    this.hooks.add(hook);
-    return () => this.hook;
+  hook(
+    action: ActionFn<T>,
+    { once = false, injectCache = true } = {}
+  ): Hook<T> {
+    let hook: Hook<T>;
+    hook = {
+      action,
+      unhook: () => {
+        this.#hooks.delete(hook);
+      },
+    };
+    this.#hooks.add(hook);
+    if (once) {
+      this.next().then(hook.unhook);
+    }
+    if (injectCache && this.#value !== nil) {
+      action(this.#value, hook);
+    }
+    return hook;
   }
 
   set(value: T) {
-    this.value = value;
-    for (const hook of this.hooks) {
-      hook(value);
+    this.#value = value;
+    for (const hook of this.#hooks) {
+      hook.action(value, hook);
+    }
+  }
+
+  get(noNil: true): Promise<T>;
+  get(noNil: false): T | Nil;
+  get(noNil = true) {
+    if (noNil && this.#value === nil) {
+      return this.next();
+    } else {
+      return this.#value;
+    }
+  }
+
+  next(): Promise<T> {
+    if (!this.#next) {
+      return (this.#next = new Promise<T>((resolve) => {
+        this.hook(resolve, { once: true });
+      }));
+    } else {
+      return this.#next;
     }
   }
 }
